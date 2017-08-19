f8a9afb79135573d503f24528cbb985d6672d4ca
vagrant <vagrant@homestead>
2017-08-19 00:40:56 +0000

    add about page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('simple-about');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
